d67d4e9a4e42a958633f30308626ce9e0a4e350c
Ron Gabriel Ramos <rongabriel01.ramos@gmail.com>
2025-04-12 22:15:46 +0800

    Renaming Folders

--- projects/webdevprojects.js
+++ projects/webdevprojects.js
@@ -13,7 +13,7 @@ for (var i = 0; i < projectpics.length; i++){
     var projectContainer = document.getElementById('projectContainer');
     projectContainer.innerHTML += `
        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 p-5" style="display:flex; justify-content: center;">
-                    <a href="webdevproj/project${1+i}/index.html">
+                    <a href="WEBDEV/project${1+i}/index.html">
                         <div class="container folder p-0"
                             style="max-width: 400px; text-align: center; position: relative; display: flex; justify-content: left; align-items: start; border-radius:20px">
                             <div class="textContainer mt-1"
