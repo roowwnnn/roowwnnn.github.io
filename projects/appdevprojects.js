e2c7d6c3d0fb24e93eab922d759deec149ade56c
Ron Gabriel Ramos <rongabriel01.ramos@gmail.com>
2025-04-28 01:16:31 +0800

    Added A03 | ADET

--- projects/appdevprojects.js
+++ projects/appdevprojects.js
@@ -1,5 +1,5 @@
 var projectpics = [
-   
+    "appdevproject2.png",
 
 
 ];
@@ -7,17 +7,18 @@ var projectpics = [
 for (var i = 0; i < projectpics.length; i++){
     var projectContainer = document.getElementById('projectContainer');
     projectContainer.innerHTML += `
-       <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 p-5" style="display:flex; justify-content: center;">
-                    <a href="projects/index.html">
-                        <div class="container folder p-0"
-                            style="max-width: 400px; text-align: center; position: relative; display: flex; justify-content: left; align-items: start; border-radius:20px">
-                            <div class="textContainer mt-1"
-                                style="background-color: #181616; height:30px; width: 115px; border-radius: 20px; position:absolute; color: white; font-size: 18px;">
-                                <p class="m-0"><b>Project ${[1+i]}</b></p>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 p-5"
+                        style="display:flex; justify-content: center;">
+                        <a href="../ADET/A0${[3+i]}/index.html">
+                            <div class="container folder p-0"
+                                style="max-width: 400px; text-align: center; position: relative; display: flex; justify-content: left; align-items: start; border-radius:20px">
+                                <div class="textContainer mt-1"
+                                    style="background-color: #181616; height:30px; width: 115px; border-radius: 20px; position:absolute; color: white; font-size: 18px;">
+                                    <p class="m-0"><b>Project ${[3+i]}</b></p>
+                                </div>
+                                <img src="../imgs/${projectpics[i]}" class="img-fluid">
                             </div>
-                            <img src="../imgs/${projectpics[i]}" class="img-fluid">
-                        </div>
-                    </a>
+                        </a>
         </div>
     `
 }
